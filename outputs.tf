e9f9afa4426b4ec88f341f2b014f6aa5e7a2fe87
stekern <stekern@users.noreply.github.com>
2020-02-26 15:50:17 +0100

    Add new output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "function_name" {
   description = "The name of the Lambda function."
   value       = aws_lambda_function.pipeline_set_version.id
 }
+
+output "lambda_exec_role_id" {
+  description = "The name of the execution role given to the lambda."
+  value       = aws_iam_role.lambda_exec.id
+}
